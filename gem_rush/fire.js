462085aa0f9716b7706afebb34ba3de32ea48ab9
Robin <blibb@connect.ust.hk>
2023-04-24 18:53:51 +0800

    Publish to github

---
+++ gem_rush/fire.js
@@ -0,0 +1,21 @@
+const Fire = function(ctx, x, y) {
+    const fire = {
+        icon: { x: 208, y:  112, width: 16, height: 16, count: 1, timing: 200, loop: true }
+    }
+
+    const sprite = Sprite(ctx, x, y);
+
+    sprite.setSequence(fire.icon)
+          .setScale(2)
+          .setShadowScale({ x: 0.75, y: 0.2 })
+          .useSheet("object_sprites.png");
+
+    return {
+        
+            
+        
+        //getBoundingBox: sprite.getBoundingBox,
+        draw: sprite.draw,
+        update: sprite.update
+    };
+}
