0ed899fce13862b8f0314c3f52ca9ff6bcf82acf
honhon13579 <113840506+honhon13579@users.noreply.github.com>
2023-05-02 01:08:39 +0800

    WebSocket implementation Known issue: log out when refreshing

--- server.js
+++ server.js
@@ -83,13 +83,9 @@ app.post("/signin", (req, res) => {
     // Sending a success response with the user account
     req.session.user = {username, name: users[username].name};
     res.json({ status: "success", user: req.session.user });
-    console.log("A user signin successfully");
-    // log the username of the user who signin
-    console.log("Who is: " + req.session.user.username);
+
+    console.log("A user signin successfully: "+ req.session.user.username);
     console.log();
- 
-    // Delete when appropriate
-    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
 });
 
 // Handle the /validate endpoint
@@ -103,20 +99,13 @@ app.get("/validate", (req, res) => {
 
     // Sending a success response with the user account
     res.json({ status: "success", user: req.session.user });
- 
-    // Delete when appropriate
-    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
 });
 
 // Handle the /signout endpoint
 app.get("/signout", (req, res) => {
 
-    console.log("A user called signout");
-    // log the username of the user who call signout
-    if (req.session.user) {
-        console.log("Who is: " + req.session.user.username);
-        console.log();
-    }
+    console.log("A user called signout: " + req.session.user.username);
+    console.log();
 
     // Deleting req.session.user
     if (req.session.user) {
@@ -124,28 +113,112 @@ app.get("/signout", (req, res) => {
     }
     // Sending a success response
     res.json({ status: "success" });
-
-    // if there is no current user, log a message
-    if (!req.session.user) {
-        console.log("No User is logged in now.");
-        console.log();
-    }
-    else {
-        console.log("Current User is: " + req.session.user.username);
-        console.log();
-    }
-
-    // Delete when appropriate
-    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
 });
 
+// app.get("/addFriend", (req, res) => {
+//     // Getting req.session.user
+//     if (!req.session.user) {
+//         res.json({ status: "error", error: "User is not logged in." });
+//         return;
+//     }
 
-//
-// ***** Please insert your Lab 6 code here *****
-//
+//     // Get the JSON data from the body
+//     const { username } = req.query;
 
+//     // Reading the users.json file
+//     const users = JSON.parse(fs.readFileSync("data/users.json"));
+
+//     // check if the username exists
+//     if (!(username in users)) {
+//         res.json({ status: "error", error: "Username does not exist." });
+//         return;
+//     }
+
+//     // check if the username is the same as the current user
+//     if (username === req.session.user.username) {
+//         res.json({ status: "error", error: "You cannot add yourself as a friend." });
+//         return;
+//     }
+
+//     // check if the username is already a friend
+//     if (username in req.session.user.friends) {
+//         res.json({ status: "error", error: "This user is already your friend." });
+//         return;
+//     }
+
+//     // Adding the new friend
+//     req.session.user.friends[username] = users[username].name;
+
+//     // Sending a success response to the browser
+//     res.json({ status: "success" });
+// });
+
+// app.get("/removeFriend", (req, res) => {
+//     // Getting req.session.user
+//     if (!req.session.user) {
+//         res.json({ status: "error", error: "User is not logged in." });
+//         return;
+//     }
+
+//     // Get the JSON data from the body
+//     const { username } = req.query;
+
+//     // Reading the users.json file
+//     const users = JSON.parse(fs.readFileSync("data/users.json"));
+
+//     // check if the username exists
+//     if (!(username in users)) {
+//         res.json({ status: "error", error: "Username does not exist." });
+//         return;
+//     }
+
+//     // check if the username is the same as the current user
+//     if (username === req.session.user.username) {
+//         res.json({ status: "error", error: "You cannot remove yourself as a friend." });
+//         return;
+//     }
+// });
+
+// Setting up web sockets with socket.io
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+const onlineUsers = {};
+
+// Use the created session
+io.use((socket, next) => {
+    gameSession(socket.request, {}, next);
+});
+
+io.on("connection", (socket) => {
+    if (socket.request.session.user) {
+        const newUser = {
+            name: socket.request.session.user.name,
+        };
+        onlineUsers[socket.request.session.user.username] = newUser;
+        io.emit("add user", JSON.stringify(newUser));
+        console.log("Online Users: ");
+        console.log(onlineUsers);
+        console.log();
+    }
+
+    socket.on("disconnect", () => {
+        delete onlineUsers[socket.request.session.user.username];
+        // io.emit("remove user", socket.request.session.user.username);
+        console.log("Online Users: ");
+        console.log(onlineUsers);
+        console.log();
+    });
+
+    socket.on("get users", () => {
+        user = JSON.stringify(onlineUsers);
+        socket.emit("users", user);
+    });
+});
 
 // Use a web server to listen at port 8000
-app.listen(8000, () => {
+httpServer.listen(8000, () => {
     console.log("The server has started...");
 });
