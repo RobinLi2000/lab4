663188593dd1264e3876b83fb3ea6b08e2c32c23
honhon13579 <113840506+honhon13579@users.noreply.github.com>
2023-05-01 21:08:40 +0800

    Authentication endpoints implementation

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.static("gem_rush"));
 app.use(express.json());
 
 // Use the session middleware to maintain sessions
-const chatSession = session({
+const gameSession = session({
     secret: "game",
     resave: false,
     saveUninitialized: false,
     rolling: true,
     cookie: { maxAge: 300000 }
 });
-app.use(chatSession);
+app.use(gameSession);
 
 // This helper function checks whether the text only contains word characters
 function containWordCharsOnly(text) {
@@ -49,10 +49,10 @@ app.post("/register", (req, res) => {
     // Adding the new user account
     const hash = bcrypt.hashSync(password, 10);
 
-    // H. Saving the users.json file
+    // Saving the users.json file
     users[username] = {name, password: hash};
 
-    // I. Sending a success response to the browser
+    // Sending a success response to the browser
     fs.writeFileSync("data/users.json", JSON.stringify(users, null, "  "));
 
     res.json({ status: "success" });
@@ -66,50 +66,77 @@ app.post("/signin", (req, res) => {
     // Get the JSON data from the body
     const { username, password } = req.body;
 
-    //
-    // D. Reading the users.json file
-    //
+    // Reading the users.json file
+    const users = JSON.parse(fs.readFileSync("data/users.json"));
 
-    //
-    // E. Checking for username/password
-    //
+    // check if the username exists
+    if (!(username in users)) {
+        res.json({ status: "error", error: "Username does not exist." });
+        return;
+    }
+    // check if the password is correct
+    if (!bcrypt.compareSync(password, users[username].password)) {
+        res.json({ status: "error", error: "Password is incorrect." });
+        return;
+    }
 
-    //
-    // G. Sending a success response with the user account
-    //
+    // Sending a success response with the user account
+    req.session.user = {username, name: users[username].name};
+    res.json({ status: "success", user: req.session.user });
+    console.log("A user signin successfully");
+    // log the username of the user who signin
+    console.log("Who is: " + req.session.user.username);
+    console.log();
  
     // Delete when appropriate
-    res.json({ status: "error", error: "This endpoint is not yet implemented." });
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
 });
 
 // Handle the /validate endpoint
 app.get("/validate", (req, res) => {
 
-    //
-    // B. Getting req.session.user
-    //
+    // Getting req.session.user
+    if (!req.session.user) {
+        res.json({ status: "error", error: "User is not logged in." });
+        return;
+    }
 
-    //
-    // D. Sending a success response with the user account
-    //
+    // Sending a success response with the user account
+    res.json({ status: "success", user: req.session.user });
  
     // Delete when appropriate
-    res.json({ status: "error", error: "This endpoint is not yet implemented." });
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
 });
 
 // Handle the /signout endpoint
 app.get("/signout", (req, res) => {
 
-    //
-    // Deleting req.session.user
-    //
+    console.log("A user called signout");
+    // log the username of the user who call signout
+    if (req.session.user) {
+        console.log("Who is: " + req.session.user.username);
+        console.log();
+    }
 
-    //
+    // Deleting req.session.user
+    if (req.session.user) {
+        delete req.session.user;
+    }
     // Sending a success response
-    //
- 
+    res.json({ status: "success" });
+
+    // if there is no current user, log a message
+    if (!req.session.user) {
+        console.log("No User is logged in now.");
+        console.log();
+    }
+    else {
+        console.log("Current User is: " + req.session.user.username);
+        console.log();
+    }
+
     // Delete when appropriate
-    res.json({ status: "error", error: "This endpoint is not yet implemented." });
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
 });
 
 
