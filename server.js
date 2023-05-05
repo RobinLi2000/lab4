4396e8cb70e1383d828e9d4a617222e610b6ee04
kckongab <90830801+Sonnnnnnny@users.noreply.github.com>
2023-05-06 02:51:55 +0800

    Modified multiple files
    Modified:
    socket.js, ui.js, register.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function containWordCharsOnly(text) {
 // Handle the /register endpoint
 app.post("/register", (req, res) => {
     // Get the JSON data from the body
-    const { username, name, password ,status, high_score} = req.body;
+    const { username, name, password ,status, elo, frd_list} = req.body;
 
     // Reading the users.json file
     const users = JSON.parse(fs.readFileSync("data/users.json"));
@@ -60,7 +60,7 @@ app.post("/register", (req, res) => {
     const hash = bcrypt.hashSync(password, 10);
 
     // add new user to users
-    users[username] = {name, password: hash, status, high_score};
+    users[username] = {name, password: hash, status, elo, frd_list};
 
     // Saving the players.json file
     fs.writeFileSync("data/users.json", JSON.stringify(users, null, "  "));
@@ -71,7 +71,7 @@ app.post("/register", (req, res) => {
 
 app.post("/signin", (req, res) => {
     // Get the JSON data from the body
-    const { username, password ,status, high_score} = req.body;
+    const { username, password } = req.body;
 
     // Reading the players.json file
     const users = JSON.parse(fs.readFileSync("data/users.json"));
@@ -91,7 +91,7 @@ app.post("/signin", (req, res) => {
     }
 
     //generate user session
-    req.session.user = {username, name: user.name, status: user.status, high_score: user.high_score};
+    req.session.user = {username, name: user.name, status: user.status, elo: user.elo, frd_list: user.frd_list};
 
     // Sending a success response with the user account
     res.json({ status: "success", user: req.session.user});
@@ -200,31 +200,36 @@ io.use((socket, next) => {
 const onlineUsers = {};
 
 io.on("connection", (socket) => {
-
+    // connecting socket
     if(socket.request.session.user){
         onlineUsers[socket.request.session.user.username] = {name: socket.request.session.user.name};
+        socket.emit("your friends", socket.request.session.user.frd_list[1]);
         io.emit("add user", JSON.stringify(socket.request.session.user));
         console.log("Online Users: ");
         console.log(onlineUsers);
         console.log();
     };
-
+    // disconnected socket
     socket.on("disconnect", () => {
-        
         delete onlineUsers[socket.request.session.user.username];
         io.emit("remove user", JSON.stringify(socket.request.session.user));
         console.log("Online Users: ");
         console.log(onlineUsers);
         console.log();
     });
-
+    // fetch online users
     socket.on("get users", () => {
         const list = JSON.stringify(onlineUsers);
         socket.emit("users", list);
     });
-
-    socket.on("send Request", (content) => {
-        
+    // fetch friend request
+    socket.on("get frd request", () => {
+        const requests = JSON.parse(fs.readFileSync("data/frd_request.json"), "utf-8");
+        socket.emit("frd requests", JSON.stringify(requests));
+    });
+    // Validation on friend request
+    socket.on("send request", (content) => {
+        // sanity checking
         console.log(content);
         const users = JSON.parse(fs.readFileSync("data/users.json"));
         // check if the username already existed
@@ -232,17 +237,35 @@ io.on("connection", (socket) => {
             socket.emit("user not exist", content);
             return;
         }
-
+        // friendzoning urself is illegal
+        if((content.localeCompare(socket.request.session.user.username) == 0)){
+            socket.emit("Stop adding yourself, you have no friends?");
+            console.log("stop it");
+            return;
+        }
+        // read request folder
+        const requestList = JSON.parse(fs.readFileSync("data/frd_request.json"));
+        // say no to repeated friend request
+        for(const frd_req of requestList){
+            console.log("frd req sender: " + frd_req.user.username);
+            console.log("frd req target: " + frd_req.content);
+            if((frd_req.user.username.localeCompare(socket.request.session.user.username) == 0) && 
+               (frd_req.content.localeCompare(content) == 0)){
+                socket.emit("repeated request");
+                console.log("repeated request");
+                return;
+            }
+        }
+        // request content
         const message = {
             user: socket.request.session.user,
             datetime: new Date(),
             content: content
         };
-
-        const chatroom = JSON.parse(fs.readFileSync("data/frd_request.json"));
-        chatroom.push(message);
-        fs.writeFileSync("data/frd_request.json", JSON.stringify(chatroom, null, "  "));
-        socket.emit("request sent");
+        // show the darn thing
+        requestList.push(message);
+        fs.writeFileSync("data/frd_request.json", JSON.stringify(requestList, null, "  "));
+        io.emit("request sent", socket.request.session.user, JSON.stringify(message));
         //io.emit("add message", JSON.stringify(message));
     });
 
